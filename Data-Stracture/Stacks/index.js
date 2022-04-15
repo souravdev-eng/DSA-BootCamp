13a4a1194cef503a1eada1706a2c03412a8d0e73
Sourav Majumdar <souravmajumdar.developer@gmail.com>
2022-04-15 06:06:16 +0530

    Complete Stack Data Stracture

---
+++ Data-Stracture/Stacks/index.js
@@ -0,0 +1,50 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      let holdingPoint = this.top;
+      this.top = newNode;
+      this.top.next = holdingPoint;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+
+    const holdingPointer = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return holdingPointer;
+  }
+}
+
+const myStack = new Stack();
+
+myStack.push("Google");
+myStack.push("Udemy");
+myStack.push("LinkedIn");
+myStack.pop();
+console.log(myStack.peek());
+
+console.log(myStack);
